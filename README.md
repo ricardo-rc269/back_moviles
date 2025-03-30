d260b8b467e8e2bcba7c2449abf4efef07989238
ricardo-rc269 <ricardocloud593@gmail.com>
2025-03-30 02:37:01 -0600

    primero

---
+++ README.md
@@ -0,0 +1,3 @@
+# backend-syntaxpad
+# back_pap
+# back_moviles
---
+++ server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const supabaseUrl = 'https://akvkayzjcazyhytbfnbz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdmtheXpqY2F6eWh5dGJmbmJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzMTM2MTgsImV4cCI6MjA1ODg4OTYxOH0.t-AsmvTGpYLWCoxxNDV6017PdlSydpRvant2WxxMFIE';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get('/registros', async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from('lugares_favoritos')
+      .select('*')
+      .order('id', { ascending: false });
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    res.status(200).json({ registros: data });
+  } catch (e) {
+    console.error('Error en GET /registros:', e);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+app.post('/registros', async (req, res) => {
+  // Extrae los datos enviados en el body
+  const { user_id, nombre, latitud, longitud, foto } = req.body;
+
+  try {
+    // Inserta el registro en la tabla "lugares_favoritos"
+    const { data, error } = await supabase
+      .from('lugares_favoritos')
+      .insert({
+        user_id, 
+        nombre,
+        latitud,
+        longitud,
+        foto,
+      })
+      .select();
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    res.status(200).json({
+      message: 'Registro agregado exitosamente',
+      registros: data,
+    });
+  } catch (e) {
+    console.error('Error en POST /registros:', e);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
